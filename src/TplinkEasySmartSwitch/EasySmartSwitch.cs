26c432b58e1bd9e245619d44290beab30fa6ac46
Gabriel Behn <gabriel.Behn@gmail.com>
2020-12-10 23:25:28 +0100

    Added a Portspeed setterfunction
    This function provides a Quick way to programmatically set a Port's Speed Limits

--- src/TplinkEasySmartSwitch/EasySmartSwitch.cs
+++ src/TplinkEasySmartSwitch/EasySmartSwitch.cs
@@ -49,7 +49,41 @@ namespace TplinkEasySmartSwitch
 		public void ClearPortStatistics()
 		{
 			// clears packet statistics and return the web-page with the new values
-			string htmlClearStat = _client.DownloadString("port_statistics_set.cgi?clear=Clear");			
+			string htmlClearStat = _client.DownloadString("port_statistics_set.cgi?clear=Clear");
+		}
+
+		/// <summary>
+		/// Sets the Provided Ports Ingress and Egress Speeds to The values (the siwth does some rounding though) 0 means unlimited
+		/// </summary>
+		/// <param name="port">The port to which to aplly these settings</param>
+		/// <param name="igrRate">Ingressrate in Kbps</param>
+		/// <param name="egrRate">Egressrate in Kbps</param>
+		public void SetPortSpeeds(int port, int igrRate, int egrRate){
+			string html = _client.DownloadString("QosBandWidthControlRpm.htm");
+			if (html.Contains("id=\"logon\""))
+			{
+				// not logged in!
+				throw new Exception("login failed");
+			}
+			var setBWData = new NameValueCollection();
+			
+			setBWData.Add("igrRate", igrRate.ToString());
+			setBWData.Add("egrRate", egrRate.ToString());
+			setBWData.Add("sel_" + port, "1");
+			setBWData.Add("applay", "Apply");
+
+			try
+			{
+				byte[] resp = _client.UploadValues("qos_bandwidth_set.cgi", "POST", setBWData);
+			}
+			catch (WebException ex)
+			{
+				//Console.WriteLine(ex);
+				// old TP-Link switches (eg.g. 1.0.2 Build 20160526 Rel.34615) reply with '401 Access Denied', but login works
+				// ex.Status==WebExceptionStatus.ProtocolError
+				// confirmed with Fiddler that this also happens in the Web-Interface -> despite the error, the switch returns html code
+			}
+
 		}
 
 		public IReadOnlyList<PortStateInfo> GetPortStatistics()
